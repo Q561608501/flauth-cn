2918656c61553b3cf71b34050ebc7c3d34819994
Q561608501 <Q561608501@users.noreply.github.com>
2026-05-11 13:37:19 +0000

    fix: remove unnecessary non-null assertion

--- lib/screens/import_export_screen.dart
+++ lib/screens/import_export_screen.dart
@@ -567,7 +567,7 @@ class _ImportExportScreenState extends State<ImportExportScreen>
               textAlign: TextAlign.center,
               style: const TextStyle(color: Colors.grey),
             ),
-            if (extra != null) extra!,
+            if (extra != null) extra,
             const SizedBox(height: 48),
 
             FilledButton.icon(
